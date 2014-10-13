551b7406ba3b2aa578df2146a85eabe41843cc1a
samer-meggaly <samer.samy.nazzir@gmail.com>
2014-10-13 06:10:31 +0200

    Initial project setup and integration with WordNet and Stanford CoreNLP tools.

---
+++ ConceptExtraction/src/test/java/libs/WordNetTest.java
@@ -0,0 +1,36 @@
+package libs;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import edu.smu.tspell.wordnet.NounSynset;
+import edu.smu.tspell.wordnet.Synset;
+import edu.smu.tspell.wordnet.SynsetType;
+import edu.smu.tspell.wordnet.WordNetDatabase;
+
+public class WordNetTest {
+
+	@Test
+	public void running() {
+		try {
+			System.setProperty("wordnet.database.dir", "./dict/");
+			NounSynset nounSynset;
+			NounSynset[] hyponyms;
+
+			WordNetDatabase database = WordNetDatabase.getFileInstance();
+			Synset[] synsets = database.getSynsets("fly", SynsetType.NOUN);
+			for (int i = 0; i < synsets.length; i++) {
+				nounSynset = (NounSynset) (synsets[i]);
+				hyponyms = nounSynset.getHyponyms();
+				System.out.println(nounSynset.getWordForms()[0] + ": "
+						+ nounSynset.getDefinition() + ") has " + hyponyms.length
+						+ " hyponyms");
+			}
+		} catch (Exception e) {
+			fail("WordNet is not running properly");
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ ConceptExtraction/src/test/java/libs/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This package holds simple classes that are used to test that 
+ * the integration of external libraries and tools is correct.
+ * Classes typically run simple execution scenarios.
+ * 
+ * @author samer
+ *
+ */
+package libs;
